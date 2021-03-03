40c8269992033da65bff4252fd809a3b16156641
Andreasjj <andreasjj@gmail.com>
2021-03-03 20:29:45 +0100

    Fixed some wrong naming

--- src/main/kotlin/com/andreasjj/controller/Controller.kt
+++ src/main/kotlin/com/andreasjj/controller/Controller.kt
@@ -2,17 +2,14 @@ package com.andreasjj.controller
 
 import com.andreasjj.repository.GameRepository
 import io.micronaut.http.HttpRequest
-import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
-import io.micronaut.http.server.util.HttpClientAddressResolver
-import java.time.Instant
 import javax.inject.Inject
 
-@Controller("/ticket")
-class TicketController {
+@Controller("/")
+class Controller {
     @Inject
     lateinit var gameRepository: GameRepository
 
